a62103eeda630c60a5edba1f883200adfec08395
Viteail <vitea.sirbu1@gmail.com>
2023-07-30 10:56:09 +0300

    add class

--- script.js
+++ script.js
@@ -45,11 +45,13 @@ submitButton.addEventListener('click', (e) => {
   }
 });
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 const addBookToLibrary = () => {
